020d237ead726eecab0cfe04e6d3eaaebba69e24
Fl0Xxx <82512540+Fl0Xxx@users.noreply.github.com>
2021-07-04 21:48:32 +0300

    add wordscount feature to reverse.html

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,4 +8,6 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    words_count = len(user_text.split())
+    dictionary = {'user_text': user_text, 'reversed_text': reversed_text, 'words_count': words_count}
+    return render(request, 'reverse.html', dictionary)
